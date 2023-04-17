6af0a8ffee37681fab85ee38cc743802014320b7
Andrey Reinwald <andreinwald@gmail.com>
2023-04-17 08:36:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php
+// Getting compiled script and styles from Webpack build folder
 $assetManifest = json_decode(file_get_contents(__DIR__ . '/build/asset-manifest.json'), true);
-$stylesFile = $assetManifest['files']['main.css'];
-$scriptsFile = $assetManifest['files']['main.js'];
+$frameworkStylesFile = $assetManifest['files']['main.css'];
+$frameworkScriptsFile = $assetManifest['files']['main.js'];
 
-register_nav_menus(
-    array(
-        'primary' => 'Primary menu',
-    )
-);
-
+// Passing to JS WordPress env variables
+$wpOptions = wp_load_alloptions();
+foreach (['admin_email', 'mailserver_url', 'mailserver_login', 'mailserver_pass', 'rewrite_rules', 'wp_user_roles', 'cron'] as $option) {
+    unset($wpOptions[$option]);
+}
+$wpVariables = [
+    'options' => $wpOptions,
+    'template_directory_uri' => get_template_directory_uri(),
+];
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <base href="<?php echo get_template_directory_uri() ?>/build/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="<?= $wpOptions['blog_charset'] ?>">
+    <base href="<?= $wpVariables['template_directory_uri'] ?>/build/">
     <title><?php wp_title('|', true, 'right'); ?></title>
     <?php wp_head(); ?>
-    <script defer="defer" src="<?php echo $scriptsFile ?>"></script>
-    <link href="<?php echo $stylesFile ?>" rel="stylesheet">
+    <script>
+        window.wpVariables = <?= json_encode($wpVariables) ?>;
+        console.log(window.wpVariables);
+    </script>
+    <link href="<?= $frameworkStylesFile ?>" rel="stylesheet">
+    <script defer="defer" src="<?= $frameworkScriptsFile ?>"></script>
 </head>
 <body>
-
 <div id="root"></div>
-
-<?php wp_footer(); ?>
+<?php wp_footer() ?>
 </html>
